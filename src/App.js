f658625319ce316f65e9ef4746a5212949cdfaf7
Gabriel <gabrieloliveiragonzaga1@hotmail.com>
2022-02-17 17:05:56 -0300

    Atividade Proposta no dia 17/02/2022

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import Primeiro from './components/Primeiro'
+import Segundo from './components/Segundo'
+import CompPadrao, {Component1, Component2} from './components/Multi'
 
-function App() {
-  return <div>
-    <header>Primeiro</header>
-  <h1>Primeiro componente</h1>
-  <body> corpo </body>
-  </div>
+function App (){
+  return(
+    <div>
+      <h3>Segundo texto</h3>
+      <Primeiro/>
+      <Segundo/>
+      <CompPadrao/> 
+      <Component1/>
+      <Component2/>
+  
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Multi.js
@@ -0,0 +1,9 @@
+export default function () {
+    return <h2>Componente Padrão</h2>
+}
+export function Component1() {
+    return <h2>Componente 1</h2>
+}
+export function Component2() {
+    return <h2>Componente 2</h2>
+}
